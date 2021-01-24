2ae5b591acfa4f4d15df78454ca1a4d73ac9da27
yves <yvesmomplais@hotmail.com>
2021-01-23 19:03:12 -0500

    commit tree 82606846a53685d9877dd564eed74e3f57739120

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+
+    attr_accessor :roster
+
+    def initialize(roster)
+    @roster = {}
+    end
+
+    
+    def add_student(student_name, grade)
+        if @roster[grade]
+          @roster[grade] << student_name
+        else
+          @roster[grade] = []
+          @roster[grade] << student_name
+        end
+    end
+   
+
+    def grade(grade)
+        self.roster[grade]
+    end
+
+
+    def sort
+        self.roster.each do |grade, students|
+            students.sort!
+        end
+    end
+
+
+end
